372a9e7fe989ffe936197cf2a8074d326d53ae92
danieldeng2 <danieldeng223@gmail.com>
2021-06-09 14:07:30 +0100

    feat(client): use thin scrollbar for modal and pages
    Co-authored-by: Sudarshan Sreeram
    <Sudarshan-Sreeram@users.noreply.github.com>

--- client/src/components/ClubModal/components/ClubBody/style.ts
+++ client/src/components/ClubModal/components/ClubBody/style.ts
@@ -5,6 +5,25 @@ const useStyles = createUseStyles((theme: any) => ({
     overflowX: `hidden`,
     overflowY: `scroll`,
     height: "100%",
+    marginRight: "4px",
+    scrollbarWidth: "thin",
+    scrollbarColor: `${theme.background} ${theme.background}`,
+    "&::-webkit-scrollbar": {
+      width: `8px`,
+    },
+    "&::-webkit-scrollbar-track": {
+      background: `${theme.background}`,
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: `${theme.background}`,
+      borderRadius: "8px",
+    },
+    "&:hover": {
+      scrollbarColor: `${theme.separator} ${theme.background}`,
+    },
+    "&:hover::-webkit-scrollbar-thumb": {
+      backgroundColor: `${theme.separator}`,
+    },
   },
   contentTab: {
     marginRight: "8px",
