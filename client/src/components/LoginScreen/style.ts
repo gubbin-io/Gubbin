bc65f01dde8618eda658a4356a0896e10b478713
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-06-12 19:45:54 +0100

    feat(client): complete login page layout

--- client/src/components/LoginScreen/style.ts
+++ client/src/components/LoginScreen/style.ts
@@ -15,6 +15,7 @@ const useStyles = createUseStyles((theme: any) => ({
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
   },
   left: {
     width: "50%",
@@ -31,21 +32,100 @@ const useStyles = createUseStyles((theme: any) => ({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    width: "50%",
+    maxWidth: "420px",
   },
   brandText: {
-    fontSize: "48px",
+    fontSize: "38px",
     fontWeight: "600",
   },
   brandIcon: {
-    width: "128px",
-    height: "128px",
+    width: "108px",
+    height: "108px",
     marginBottom: "16px",
   },
   brandTagline: {
-    fontSize: "20px",
+    fontSize: "18px",
     fontWeight: "400",
     color: "#ADB5BD",
-    fontStyle: "italic",
+  },
+  form: {
+    marginTop: "48px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    width: "100%",
+    "& > div": {
+      width: "100%",
+    },
+    marginBottom: "32px",
+  },
+  field: {
+    backgroundColor: theme.background,
+    borderRadius: theme.radius,
+    "&::placeholder": {
+      color: "#ADB5BD",
+      opacity: 1,
+    },
+  },
+  miscText: { marginTop: "8px", color: "#ADB5BD" },
+  link: {
+    color: "#868e96",
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "none",
+      color: "#000000",
+    },
+  },
+  label: {
+    fontSize: "14px",
+    color: "#868e96",
+    fontWeight: "500",
+  },
+  submitButton: {
+    width: "100%",
+    backgroundColor: "#000000", //Note: Change later to use theme
+    color: "#FFFFFF", // Note: Change later to use theme
+
+    borderRadius: theme.radius,
+    borderWidth: 0,
+    padding: 8,
+    marginTop: 16,
+
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+
+    fontWeight: `500`,
+    fontSize: `15px`,
+    textAlign: `left`,
+
+    "&:active": {
+      backgroundColor: `#1971c2 !important`,
+      color: theme.invertText,
+      fontWeight: `500`,
+    },
+    "&.active": {
+      backgroundColor: `#1971c2 !important`,
+      color: theme.invertText,
+      fontWeight: `500  !important`,
+    },
+    "&:hover": {
+      backgroundColor: "#495057",
+      color: theme.invertText,
+    },
+    "&:focus:hover": {
+      backgroundColor: "#495057",
+      color: theme.invertText,
+    },
+    "&:focus": {
+      backgroundColor: "#495057",
+      color: theme.invertText,
+    },
+
+    transition: `0.2s background`,
+    WebkitTransition: `0.2s background`,
+    MozTransition: `0.2s background`,
   },
 }));
 
