5ed44667deb9d59f43704f07008c2165edde5ac5
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-06-15 04:58:08 +0100

    feat(client): implement featured society rolling carousel

--- client/src/components/DiscoverPage/components/FeaturedSocieties/index.tsx
+++ client/src/components/DiscoverPage/components/FeaturedSocieties/index.tsx
@@ -1,10 +1,12 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
+import { Card, Col, Row } from "react-bootstrap";
 import Carousel from "react-bootstrap/esm/Carousel";
+import { classicNameResolver } from "typescript";
 import { GET_CLUB_COLLECTION } from "../../../../constants/queries";
 import { Club } from "../../../../constants/types";
 import LoadingScreen from "../../../LoadingScreen";
-// import useStyles from "./style";
+import useStyles from "./style";
 
 export interface FeaturedSocietiesProp {
   showModalClub: (id: string) => void;
@@ -17,29 +19,42 @@ const FeaturedSocieties: React.FC<FeaturedSocietiesProp> = ({
   const { loading, error, data } = useQuery(GET_CLUB_COLLECTION, {
     variables: { collectionId: "60c7bb57889e63a7783c260f" },
   });
+  const classes = useStyles();
 
   if (loading) return <LoadingScreen />;
   if (error || data.clubCollection.clubs.length === 0) return <></>;
 
   return (
-    <Carousel interval={null} controls={false}>
-      {data.clubCollection.clubs.map(
-        ({ id, clubName, description, backgroundUri }: Club) => (
-          <Carousel.Item key={id}>
-            <img
-              className="d-block w-100"
-              src={backgroundUri}
-              alt={clubName}
-              style={{ width: 800, height: 400 }}
-            />
-            <Carousel.Caption>
-              <h3>{clubName}</h3>
-              <p>{description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        )
-      )}
-    </Carousel>
+    <div className={classes.container}>
+      <Row className={classes.carousel}>
+        {data.clubCollection.clubs.map(
+          ({ id, clubName, description, backgroundUri }: Club) => (
+            <Col
+              className={classes.carouselItem}
+              key={id}
+              xs={12}
+              sm={12}
+              md={12}
+              lg={8}
+              xl={5}
+            >
+              <div className={classes.featureCard}>
+                <img
+                  className={classes.image}
+                  src={backgroundUri}
+                  alt={clubName}
+                />
+                <div className={classes.overlay}></div>
+                <div className={classes.clubDetails}>
+                  <span className={classes.clubName}>{clubName}</span>
+                  <span className={classes.clubTagline}>{description}</span>
+                </div>
+              </div>
+            </Col>
+          )
+        )}
+      </Row>
+    </div>
   );
 };
 
